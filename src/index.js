ae5d24d29d54dbd871ab2c4046268a3403df53cf
spectr-e <devs.josia@gmail.com>
2022-12-04 03:09:45 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
-}
-
-document.addEventListener('DOMContentLoaded', init);
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = e.target.searchByID.value;
+    console.log(input);
+    fetch(`http://localhost:3000/movies/${input}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
+document.addEventListener("DOMContentLoaded", init);
